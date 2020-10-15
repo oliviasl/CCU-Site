f780de6687f84a8d85140f4754de7da35f71681d
Olivia Lai <olivialai886@gmail.com>
2020-10-15 00:55:01 -0700

    Make about and announcements page
    Add js for animations

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    $(".banner-header").css("animation","fadeHeader .75s ease-in-out");
+    $(".banner-header").css("opacity","1");
+    $(".banner-overlay").css("animation","fadeBannerOverlay .75s ease-in-out");
+    $(".banner-overlay").css("opacity",".6");
+    $(".banner-header-home").css("animation","fadeHeader 1s ease-in-out");
+    $(".banner-header-home").css("opacity","1");
+};
